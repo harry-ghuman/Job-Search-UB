4937e46812e47777970e9af584f4bb8350319571
Harpuneet Ghuman <harpuneetghuman@hotmail.com>
2017-04-14 15:00:51 -0400

    built header and banner of homepage

--- header.php
+++ header.php
@@ -4,29 +4,33 @@
     <title>Jobs Portal | University of Bridgeport</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro|Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="index.php">Jobs Portal | UB</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li><a href="index.php">Home</a></li>
-				<li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Login<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-						<li><a href="admin_login.php">Admin</a></li>
-						<li><a href="teacher_login.php">Teacher</a></li>
-						<li><a href="student_login.php">Student</a></li>
-                    </ul>
-                </li>
+    <div id="header">
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <div class="logo">
+                    <a href="index.php"><img src="images/logo.png" alt="image"></a>
+                </div>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="index.php">About</a></li>
+                        <li><a href="index.php">Contact</a></li>
+        				<li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Login<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+        						<li><a href="admin_login.php">Admin</a></li>
+        						<li><a href="teacher_login.php">Teacher</a></li>
+        						<li><a href="student_login.php">Student</a></li>
+                            </ul>
+                        </li>
 
-            </ul>
+                    </ul>
+            </nav>
         </div>
-    </nav>
-</body>
-</html>
+    </div>
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-include "header.php";
-echo 'Welcome to UB\'s Job Portal';
+    <?php
+    include "header.php";
+    ?>
+    <div id="home">
+        <div class="banner">
+            <div class="title">
+                Jobs Portal | School of Computer Science
+            </div>
+            <div class="title-blurb">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do, consectetur adipisicing elit, sed do eiusmod tempor
+            </div>
+        </div>
+    </div>
+</body>
+</html>
